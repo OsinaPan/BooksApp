9f755abab0d4dfeaebb98af9af6edd66d2c13338
Michal Osinski <michal.osinski94@gmail.com>
2023-01-23 19:05:23 +0100

    add render function

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,29 @@
 {
-  'use strict';
+  ('use strict');
 
   const select = {
     templateOf: {
       book: '#template-book',
     },
+
     containerOf: {
       booksList: '.books-list',
-      filters: '.filters',
-    },
-    book: {
-      image: '.books-list .book_image',
     },
   };
+  const templates = {
+    books: Handlebars.compile(
+      document.querySelector(select.templateOf.book).innerHTML
+    ),
+  };
 
+  const render = function () {
+    for (const book of dataSource.books) {
+      const generatedHTML = templates.books(book);
+      const generateDOMElement = utils.createDOMFromHTML(generatedHTML);
+      const booksContainer = document.querySelector(select.containerOf.booksList);
+      booksContainer.appendChild(generateDOMElement);
+    }
+  };
+
+  render();
 }
