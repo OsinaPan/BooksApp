97ad058732facf826dd64f5744f5ac19471a2a90
Michal Osinski <michal.osinski94@gmail.com>
2023-01-23 18:54:00 +0100

    start

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+{
+  'use strict';
+
+  const select = {
+    templateOf: {
+      book: '#template-book',
+    },
+    containerOf: {
+      booksList: '.books-list',
+      filters: '.filters',
+    },
+    book: {
+      image: '.books-list .book_image',
+    },
+  };
+
+}
